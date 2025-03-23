9989a60dcd53561771df3df67319914a6d3803a4
svc_ec_git_devext <priyesh.nd@logixal.com>
2025-03-23 11:36:40 +0530

    added a todo comment

--- Test.js
+++ Test.js
@@ -4,6 +4,7 @@
  * @returns {Number}
  */
 function Add(a, b) {
+  // TODO : add comment for below code
   if(typeof a !== "number" && typeof b !== "number") return;
   const sum = a + b;
   return sum;
